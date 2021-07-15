d400cadeb7e99017873d1f307e434001575a9849
Leonardo Cabral <leonardomcabral019@gmail.com>
2021-07-15 17:50:38 -0300

    :construction: Added CardFrontBack in BoardGame

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
-import BoardGame from './src/objects/BoardGame'
+
 import ScoreBoard from './src/objects/ScoreBoard'
+import BoardGame from './src/objects/BoardGame'
 
 const $root = document.querySelector('#root') // $ = toda variável com $ guarda um elemento da tela.
 
@@ -10,7 +11,7 @@ $root.insertAdjacentHTML(
     "beforeend", 
     `
         ${ScoreBoard()}   
-        ${BoardGame(6)}    
+        ${BoardGame(2)}    
     `)  // insertAdjacentHTML = específico da API DOM
         // componente deve ser isolado para ser reutilizado outras vezes.
         // BoardGame(6) -> o '6' é o parâmetro onde se configura quantas cartas quer ter no jogo
