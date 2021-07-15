c1a7cc4b1e44111db76da27536870f4e26c7bfda
Leonardo Cabral <leonardomcabral019@gmail.com>
2021-07-15 16:37:45 -0300

    :construction: Create PlayerName component without CSS

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
 import BoardGame from './src/objects/BoardGame'
+import PlayerName from './src/components/PlayerName'
 
 const $root = document.querySelector('#root') // $ = toda variável com $ guarda um elemento da tela.
-const $htmlBoardGame = BoardGame(6) // aqui se configura quantas cartas se quer ter no jogo
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame) // específico da API DOM
-// componente deve ser isolado para ser reutilizado outras vezes.
+$root.insertAdjacentHTML(
+    "beforeend", 
+    `
+        ${PlayerName('Player 1')}
+        ${PlayerName('Player 2')}
+        ${BoardGame(6)}    
+    `)  // insertAdjacentHTML = específico da API DOM
+        // componente deve ser isolado para ser reutilizado outras vezes.
+        // BoardGame(6) -> o '6' é o parâmetro onde se configura quantas cartas quer ter no jogo
